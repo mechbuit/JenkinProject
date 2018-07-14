6ea7b94ffc2e14517a25121d4f5f906163c0bfda
mechbuit <mech.rajesh01@gmail.com>
2018-07-14 15:55:01 +0530

    commit

---
+++ src/test/java/jenkin/Jenkin.java
@@ -0,0 +1,20 @@
+package jenkin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Jenkin {
+	
+	@Test
+	public void test1()
+	{
+		System.setProperty("webdriver.chrome.driver", ".\\Resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		System.out.println("google opened");
+		driver.quit();
+	}
+
+}
